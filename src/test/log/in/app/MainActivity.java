714c8820cdbc5e6f1034bb3779b35e6f0fcb8713
hsy <che@gmail.com>
2014-08-08 15:33:29 +0800

    add logcat in app test

---
+++ src/test/log/in/app/MainActivity.java
@@ -0,0 +1,90 @@
+
+package test.log.in.app;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Another great log cat open source project https://github.com/cheyiliu/Catlog
+ */
+public class MainActivity extends Activity {
+
+    private boolean mStop = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Process mLogcatProc = null;
+                BufferedReader reader = null;
+                BufferedWriter writer = null;
+                try {
+                    mLogcatProc = Runtime.getRuntime().exec(new String[] {
+                            // "logcat", "WindowManager:I *:S"
+                            "logcat", ""
+                    });
+                    reader = new BufferedReader(new InputStreamReader(mLogcatProc.getInputStream()));
+                    File tmp = new File(getCacheDir() + File.separator
+                            + "tmpLogInApp");
+                    if (tmp.exists()) {
+                        tmp.delete();
+                    }
+                    tmp.createNewFile();
+                    OutputStream out = new FileOutputStream(tmp);
+                    writer = new BufferedWriter(new OutputStreamWriter(out));
+
+                    String line;
+                    while (true) {
+                        if (mStop) {
+                            break;
+                        }
+                        line = reader.readLine();
+                        writer.newLine();
+                        writer.write(line);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                    try {
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        if (writer != null) {
+                            writer.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mStop = true;
+    }
+
+}
