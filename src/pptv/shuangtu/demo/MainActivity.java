6fb9f228e3e5f4843a8fbd9e884eea2f6f4c30c8
hsy <che@gmail.com>
2014-08-14 15:04:35 +0800

    add demo pptv shuangtu, step 1

---
+++ src/pptv/shuangtu/demo/MainActivity.java
@@ -0,0 +1,17 @@
+
+package pptv.shuangtu.demo;
+
+import com.test4android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_pptv_shuangtu_demo);
+    }
+
+}
