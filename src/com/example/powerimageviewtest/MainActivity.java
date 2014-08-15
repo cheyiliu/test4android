a43ab80415bdffe8fb39a6705a77d9c17396c91f
hsy <che@gmail.com>
2014-08-15 18:09:38 +0800

    add gif imageview

---
+++ src/com/example/powerimageviewtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.powerimageviewtest;
+
+import com.test4android.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gif_imageview_activity_main);
+	}
+
+}
