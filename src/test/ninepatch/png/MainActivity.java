ab952e60521c8fc602282e4d6d93e616413c7c9f
hsy <che@gmail.com>
2014-08-22 12:42:30 +0800

    add .9 png test

---
+++ src/test/ninepatch/png/MainActivity.java
@@ -0,0 +1,18 @@
+
+package test.ninepatch.png;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.test4android.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_nine_patch_png);
+
+    }
+
+}
